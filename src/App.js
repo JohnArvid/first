9a3a0b57fbcb41addf6233bb247a711e650e48a5
JohnArvid <arvid-lindstrom@live.se>
2024-02-09 13:43:36 +0100

    api call to fetch names and id's

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
 import {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
 
-    this.state = {
-      monsters: [
-        {
-          name: 'Linda',
-          id: '21t43g3e'
-        },
-        {
-          name: 'Frank',
-          id: '31t43g3e'
-        },
-        {
-          name: 'Jacky',
-          id: '41t43g3e'
-        },
-        {
-          name: 'Andrei',
-          id: '51t43g3e'
-        }
+    // https://jsonplaceholder.typicode.com/users
 
-      ]
-    }
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => this.setState(() => {
+        return {monsters: users}
+      }));
   }
 
   render() {
