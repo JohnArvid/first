9b9420754a5b69bcf467fb26b1069079f5aa90db
JohnArvid <arvid-lindstrom@live.se>
2024-02-09 15:10:34 +0100

    imported cardlist component to app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import {Component} from 'react';
+
+import CardList from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -46,11 +48,13 @@ class App extends Component {
         className='search-box' 
         type='search' 
         placeholder='Search monsters'/>
-        {
+        
+        {/* {
           filteredMonsters.map((monster) => {
             return <div key={monster.id}><h1>{monster.name}</h1></div>;
           })
-        }
+        } */}
+        <CardList />
       </div>
     );
   }
