3460ba618f35df18771a6d33323e0d9b85eafb0a
JohnArvid <arvid-lindstrom@live.se>
2024-02-09 14:34:05 +0100

    Moved filtering to state and stuff

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: '',
     };
     console.log('constructor');
   }
@@ -24,23 +25,24 @@ class App extends Component {
 
   render() {
     console.log('render');
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
         <input onChange={(e) => {
-          const searchString = e.target.value.toLocaleLowerCase();
-          const filteredMonsters = this.state.monsters.filter((monster) => {
-            return monster.name.toLocaleLowerCase().includes(searchString);
-          });
-
+          const searchField = e.target.value.toLocaleLowerCase();
           this.setState(() => {
-            return { monsters: filteredMonsters }
+            return { searchField }
           });
         }} 
         className='search-box' 
         type='search' 
         placeholder='Search monsters'/>
         {
-          this.state.monsters.map((monster) => {
+          filteredMonsters.map((monster) => {
             return <div key={monster.id}><h1>{monster.name}</h1></div>;
           })
         }
