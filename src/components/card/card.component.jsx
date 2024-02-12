eff276ea19949211e403e9da0d905f6e88d4123b
John Arvid <arvid-lindstrom@live.se>
2024-02-12 13:59:38 +0100

    card component created

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+
+class Card extends Component {
+
+    render() {
+        const {name, email, id} = this.props.monster;
+        return (
+            <div 
+            key= {id}
+            className='card-container'
+          >
+            <img 
+              alt={`monster  ${name}`}
+              src={`https://robohash.org/${id}?set=set3&size=180x180`} 
+            />
+            <h2>{name}</h2>
+            <p>{email}</p>
+          </div>
+        )
+    }
+}
+
+export default Card;
