45fc0bb27882f70d4958a071433f86f2181b321b
John Arvid <arvid-lindstrom@live.se>
2024-02-12 13:10:28 +0100

    expanded cardlist component and destructured props

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -7,9 +7,23 @@ class CardList extends Component {
       
       return (
         <div>
-          {monsters.map(monster => (
-            <h1  key={monster.id} >{monster.name}</h1>
-          ))}
+          {monsters.map(monster => {
+            const {name, email, id} = monster;
+
+            return (
+              <div 
+                key= {id}
+                className='card-container'
+              >
+                <img 
+                  alt={`monster  ${name}`}
+                  src={`https://robohash.org/${id}?set=set3&size=180x180`} 
+                />
+                <h2>{name}</h2>
+                <p>{email}</p>
+              </div>
+            )
+          })}
         </div>
       );
     }
