fd1dde79371a3a2710cf244fe651b8a441dfd5fc
unknown <arvid.lindstrom>
2024-02-12 11:03:35 +0100

    moved search box to component

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,1 +1,16 @@
 import { Component } from "react";
+
+class SearchBox extends Component {
+  render() {
+    return (
+      <input 
+        onChange={onSearchChange} 
+        className='search-box' 
+        type='search' 
+        placeholder='Search monsters'
+      />
+    )
+  }
+}
+
+export default SearchBox;
