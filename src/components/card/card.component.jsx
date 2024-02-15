65c51bf3efca4157a1dd1bdf3c2e6fe4a7452ee5
JohnArvid <arvid-lindstrom@live.se>
2024-02-15 13:28:02 +0100

    small destructuring and indentations

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,20 +3,21 @@ import './card.styles.css';
 
 const Card = ({robot}) => {
   
+  const {id, name, email} = robot;
+
   return (
     <div 
-    key= {robot.id}
+    key= {id}
     className='card-container'
   >
     <img 
-      alt={`robot  ${robot.name}`}
-      src={`https://robohash.org/${robot.id}?set=set3&size=180x180`} 
+      alt={`robot  ${name}`}
+      src={`https://robohash.org/${id}?set=set3&size=180x180`} 
     />
-    <h2>{robot.name}</h2>
-    <p>{robot.email}</p>
+    <h2>{name}</h2>
+    <p>{email}</p>
   </div>
   )
-
 }
 
 export default Card;
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,14 +1,14 @@
 import './search-box.styles.css'
 
 const SearchBox = (props) => {
-    return (
-      <input
-        type='search'
-        className={`search-box ${props.className}`}
-        placeholder={props.placeholder}
-        onChange={props.onChangeHandler} 
-      />
-    )
-  }
+  return (
+    <input
+      type='search'
+      className={`search-box ${props.className}`}
+      placeholder={props.placeholder}
+      onChange={props.onChangeHandler} 
+    />
+  )
+}
 
 export default SearchBox;
