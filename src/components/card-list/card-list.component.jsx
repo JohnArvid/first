3cd25bdaa1273b5e6d772edcfaf0979a126b0fa9
JohnArvid <arvid-lindstrom@live.se>
2024-02-15 13:17:56 +0100

    card-list working functional component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,24 +1,13 @@
 import Card from '../card/card.component';
 import './card-list.styles.css';
 
-const CardList = () => {
-    
-  const { robots } = this.props;
-  
-  return (
-    <div className='card-list'>
-      {robots.map(robot => {
-        const {name, email, id} = robot;
-        
-        return (
-          <Card 
-            robot = {robot}
-          /> 
-        )
-      })}
-    </div>
-  );
-}
+const CardList = ({ robots }) => (
+  <div className='card-list'>
+    {robots.map(robot => {
+      return <Card robot = {robot} /> 
+    })}
+  </div>
+);
 
 
 export default CardList;
